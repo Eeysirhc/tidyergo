20643bdbcbb09c011fca707665f5056952e1c21d
Eeysirhc <cyee428@gmail.com>
2022-04-02 13:27:09 -0700

    add ergodex functions

---
+++ R/amm.R
@@ -0,0 +1,84 @@
+# ErgoDEX pool statistics
+
+
+# https://api.ergodex.io/v1/docs/#operation/Platform%20stats
+#' Platform Statistics
+#'
+#' Get statistics on whole AMM.
+#'
+#' @return A tibble with platform statistics for volume and TVL.
+#' @import dplyr
+#' @import httr
+#' @import jsonlite
+#' @import purrr
+#' @import tidyr
+#' @export
+#'
+#' @examples
+#' ergodexPlatformStats()
+
+ergodexPlatformStats <- function(){
+
+  url_request <- paste0("https://api.ergodex.io/v1/amm/platform/stats")
+
+  df <- GET(url_request) %>%
+    content(as = "text", encoding = "UTF-8") %>%
+    jsonlite::fromJSON(flatten = TRUE) %>%
+    purrr::map(unlist) %>%
+    as_tibble() %>%
+    tidyr::pivot_longer(1:2) %>%
+    group_by(name) %>%
+    mutate(category = row_number()) %>%
+    ungroup() %>%
+    mutate(category = case_when(category == 1 ~ "value",
+                                category == 2 ~ "currency",
+                                category == 3 ~ "decimals")) %>%
+    arrange(name) %>%
+    tidyr::pivot_wider(names_from = category, values_from = value)
+
+  return(df)
+}
+
+
+
+
+
+# https://api.ergodex.io/v1/docs/#operation/All%20pools%20stats
+#' All Pool Stats
+#'
+#' Get statistics on all liquidity pools.
+#'
+#' @param from Window lower bound (UNIX timestamp millis).
+#' @param to Window upper bound (UNIX timestamp millis).
+#'
+#' @return A tibble with all liquidity pool statistics.
+#' @import dplyr
+#' @import httr
+#' @import jsonlite
+#' @export
+#'
+#' @examples
+#' ergodexAllPoolStats()
+#'
+#' start <- 1648796400000
+#' end <- 1648926788038
+#' ergodexAllPoolStats(from = start, to = end)
+
+ergodexAllPoolStats <- function(from = NULL, to = NULL){
+
+  url_request <- paste0("https://api.ergodex.io/v1/amm/markets",
+                        ifelse(!is.null(from),
+                               paste0("?from=", from, "&to=", to), ""))
+
+  df <- GET(url_request) %>%
+    content(as = "text", encoding = "UTF-8") %>%
+    jsonlite::fromJSON(flatten = TRUE) %>%
+    as_tibble()
+
+  return(df)
+}
+
+
+
+
+
--- README.md
+++ README.md
@@ -13,31 +13,51 @@ devtools::install_github("Eeysirhc/tidyergo")
 
 ## Usage
 
-### Current balance for an address
+### ErgoWatch
+
+#### Current balance for an address
 ```r
 bearwhale <- "9hyDXH72HoNTiG2pvxFQwxAhWBU8CrbvwtJDtnYoa4jfpaSk1d3"
 addressesBalance(bearwhale)
 ```
 
-### Balance history of an address
+#### Balance history of an address
 ```r
 bearwhale <- "9hyDXH72HoNTiG2pvxFQwxAhWBU8CrbvwtJDtnYoa4jfpaSk1d3"
 addressesBalanceHistory(bearwhale)
 ```
 
-### Current P2PK address count
+#### Current P2PK address count
 ```r
 # addresses holding >= 1M+ $MiGoreng
 migoreng <- "0779ec04f2fae64e87418a1ad917639d4668f78484f45df962b0dec14a2591d2"
 p2pkCount(bal_ge = 1000000, token_id = migoreng)
 ```
 
-
-### Token supply: emitted, circulating, and burned
+#### Token supply: emitted, circulating, and burned
 ```r
 migoreng <- "0779ec04f2fae64e87418a1ad917639d4668f78484f45df962b0dec14a2591d2"
 tokensSupply(migoreng)
 ```
+
+#### P2PK address ranking
+```r
+bearwhale <- "9hyDXH72HoNTiG2pvxFQwxAhWBU8CrbvwtJDtnYoa4jfpaSk1d3"
+p2pkRanking(bearwhale)
+```
+
+### ErgoDEX
+
+#### Retrieve AMM platform statistics
+```r
+ergodexPlatformStats()
+```
+
+#### Retrieve statistics for all liquidity pools
+```r
+ergodexAllPoolStats()
+```
+
 
 ## Roadmap
 
